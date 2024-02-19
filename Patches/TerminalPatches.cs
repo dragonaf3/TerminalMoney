177d29c64c18f9e1c2bc145f3904a91555e9d46e
drago <97@dragonaf.eu>
2024-02-19 02:36:04 +0100

    initial commit

---
+++ Patches/TerminalPatches.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+
+namespace TerminalMoney.Patches
+{
+    [HarmonyPatch(typeof(Terminal), nameof(Terminal.RunTerminalEvents))]
+    public static class TerminalPatches
+    {
+        [HarmonyPostfix]
+        static void TerminalMoney(ref int ___groupCredits)
+        {
+            if (Plugin.MoneyCommandActivated)
+            {
+                // Setzt die Credits nur, wenn der Befehl aktiviert wurde
+                ___groupCredits = 1000000;
+                // Optional: Zustand zurücksetzen, falls einmalige Anwendung gewünscht
+                Plugin.MoneyCommandActivated = false;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+using TerminalApi.Classes;
+using static TerminalApi.TerminalApi;
+
+namespace TerminalMoney
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("atomic.terminalapi", MinimumDependencyVersion: "1.5.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static bool MoneyCommandActivated = false;
+        
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+            AddCommand("rich",
+                new CommandInfo
+                {
+                    Title = "Rich",
+                    Category = "other",
+                    Description = "Adds 1.000.000 Money :D",
+                    DisplayTextSupplier = OnMoneyCommand
+                });
+        }
+
+        private String OnMoneyCommand()
+        {
+            MoneyCommandActivated = true;
+            return "1.000.000 money added \n";
+        }
+    }
+}
